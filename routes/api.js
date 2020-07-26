9c04d527f95647d7a025c2a4f714d19ebf9f2dcc
sidemt <25644062+sidemt@users.noreply.github.com>
2020-07-26 22:47:49 +0900

    Post a reply

--- routes/api.js
+++ routes/api.js
@@ -12,6 +12,7 @@ var expect = require('chai').expect;
 
 const ThreadHandler = require('../controllers/handlers/ThreadHandler.js');
 const ReplyHandler = require('../controllers/handlers/ReplyHandler.js');
+// const {ObjectID} = require('mongodb');
 
 // MongoDB setup
 const MongoClient = require('mongodb').MongoClient;
@@ -42,5 +43,31 @@ module.exports = function(app) {
         });
       });
 
-  app.route('/api/replies/:board');
+  app.route('/api/replies/:board')
+      .post(function(req, res) {
+        console.log('REQUEST:', req.body);
+        const now = (new Date()).toISOString();
+        const reply = {
+          _id: new ObjectId(),
+          text: req.body.text,
+          created_on: now,
+          reported: false,
+          delete_password: req.body.delete_password,
+        };
+        // post a new reply
+        MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, client) {
+          const db = client.db('message-board');
+          const collection = db.collection('threads');
+          collection.findOneAndUpdate({'_id': ObjectId(req.body.thread_id)},
+              {
+                $push: {replies: reply},
+                $set: {bumped_on: now},
+              },
+              function(err, doc) {
+                console.log('Saved:', doc);
+                // res.redirect to thread page /b/{board}/{thread_id}
+                res.redirect(`/b/${doc.value.board}/${ObjectId(doc.value._id)}`);
+              });
+        });
+      });
 };
