27ac492720822f39cdfa344e5309c8b267a2802f
sidemt <25644062+sidemt@users.noreply.github.com>
2020-07-26 23:47:18 +0900

    Get threads

--- routes/api.js
+++ routes/api.js
@@ -21,6 +21,36 @@ const MONGODB_CONNECTION_STRING = process.env.DB;
 
 module.exports = function(app) {
   app.route('/api/threads/:board')
+      .get(function(req, res) {
+        console.log(':board', req.params.board);
+        MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, client) {
+          const db = client.db('message-board');
+          const collection = db.collection('threads');
+          collection.find({'board': req.params.board})
+              .sort({bumped_on: -1}) // descending
+              .limit(10)
+              .toArray(function(err, docs) {
+                console.log('Docs:', docs);
+                docs.map(function(thread) {
+                  // most recent 3 replies
+                  thread.replies = thread.replies.sort(function(a, b) {
+                    return Date.parse(b.created_on) - Date.parse(a.created_on);
+                  }).slice(0, 3);
+                  thread.replies.map(function(reply) {
+                    // Don't send reported and delete_passwords fields
+                    delete reply.delete_password;
+                    delete reply.reported;
+                  });
+                  // Don't send reported and delete_passwords fields
+                  delete thread.delete_password;
+                  delete thread.reported;
+                });
+                res.json(docs);
+                db.close();
+              });
+        });
+      })
+
       .post(function(req, res) {
         console.log('REQUEST:', req.body);
         const now = (new Date()).toISOString();
@@ -65,7 +95,6 @@ module.exports = function(app) {
               },
               function(err, doc) {
                 console.log('Saved:', doc);
-                // res.redirect to thread page /b/{board}/{thread_id}
                 res.redirect(`/b/${doc.value.board}/${ObjectId(doc.value._id)}`);
               });
         });
